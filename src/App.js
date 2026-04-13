cbb0752721daf768e3608859143accbb26d999aa
daraeprincess <yudarae72@gmail.com>
2026-04-13 16:52:33 +0900

    랭킹 변동없음 아이콘 수정

--- src/App.js
+++ src/App.js
@@ -529,10 +529,14 @@ function App() {
                 <li key={index}>
                   <span className="rank">{item.rank}</span>
                   <span className="name">{item.keyword}</span>
-                  <span className={`trend trend-${trendDirection}`} aria-label={trendDirection}>
-                    <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M12 8L6 14H18L12 8Z" fill="currentColor" />
-                    </svg>
+                  <span className={`trend trend-${trendDirection}`} aria-label={trendDirection === 'neutral' ? 'same' : trendDirection}>
+                    {trendDirection === 'neutral' ? (
+                      <span className="trend-neutral-mark" aria-hidden="true">-</span>
+                    ) : (
+                      <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 8L6 14H18L12 8Z" fill="currentColor" />
+                      </svg>
+                    )}
                   </span>
                 </li>
               );
